f73a06cc00b6283daf24ca3f172a517246ba09e1
Timothy Wang <wangtim1996@gmail.com>
2017-08-26 01:24:44 -0700

    player health/take damage

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using UniRx.Triggers; // need UniRx.Triggers namespace
+
+public class PlayerHealth : MonoBehaviour
+{
+
+    public int currHealth = 2;
+    public bool isDead
+    {
+        get { return currHealth <= 0; }
+    }
+
+    public void Awake()
+    {
+        gameObject.AddComponent<ObservableTrigger2DTrigger>()
+            .OnTriggerEnter2DAsObservable()
+            .Subscribe(coll =>
+            {
+                print(gameObject.name + " Colliding with " + coll.gameObject.name);
+            });
+    }
+
+    public void TakeDamage(int dmg)
+    {
+        currHealth -= dmg;
+    }
+}
---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour {
+
+    //return false = at least one character alive
+    public bool CheckAlive()
+    {
+        foreach (Transform child in transform)
+        {
+            PlayerHealth shade = child.GetComponent<PlayerHealth>();
+            if (shade != null)
+            {
+                if (!shade.isDead)
+                {
+                    return false;
+                }
+            }
+
+        }
+        return true;
+    }
+}
