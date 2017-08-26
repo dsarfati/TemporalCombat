fd616f4642ad9ef685030ff7e639ca772ed03d65
Timothy Wang <wangtim1996@gmail.com>
2017-08-26 08:38:08 -0700

    hud controller to able add any amount of players

---
+++ Assets/Scripts/HUDController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HUDController : MonoBehaviour {
+
+    public GameObject playerStats;
+
+	// Use this for initialization
+	void Awake () {
+        AddPlayers(4);
+	}
+
+
+    void AddPlayers(int x)
+    {
+        for(int i = 0; i < x; i++)
+        {
+            Instantiate(playerStats, transform);
+        }
+    }
+}
