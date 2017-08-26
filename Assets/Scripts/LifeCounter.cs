cde3fd25faa5defadde4cca575c487dfa38b406c
Timothy Wang <wangtim1996@gmail.com>
2017-08-26 04:04:19 -0700

    starting health bar

---
+++ Assets/Scripts/LifeCounter.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LifeCounter : MonoBehaviour {
+
+    public int maxLives = 4;
+    public GameObject heart;
+
+	// Use this for initialization
+	void Awake () {
+		for(int i = 0; i < maxLives; i++)
+        {
+            Instantiate(heart, transform);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    
+}
