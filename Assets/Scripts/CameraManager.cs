99ceafe969a0aebfb6126e1b5246a5e2ed238d37
sid fernandez <angelsinhbfs@gmail.com>
2017-08-27 13:49:44 -0700

    camera follow no longer shows below the level

--- Assets/Scripts/CameraManager.cs
+++ Assets/Scripts/CameraManager.cs
@@ -10,10 +10,15 @@ namespace Assets.Scripts
         public Transform CameraTrans;
         public Vector3 TargetPos;
         public float CamSpeed = 25f;
+        public float ViewportMinOffset;
 
         private void Awake()
         {
-            var lowerConst = Mathf.Sin(20f * Mathf.Deg2Rad);
+            var vertSin = Mathf.Sin(20f * Mathf.Deg2Rad);
+            var radAngle = 40 * Mathf.Deg2Rad;
+            var halfRadHFOV = Mathf.Atan(Mathf.Tan(radAngle / 2) * (16f/9f));
+            var horiSin = Mathf.Sin(halfRadHFOV);
+            //var FrustrumMin = 
             TargetPos = CameraTrans.position;
             this.ReceiveAll<PositionUpdate>().Buffer(10).Subscribe(poss =>
             {
@@ -24,20 +29,37 @@ namespace Assets.Scripts
                 var maxY = float.MinValue;
                 for (int i = 0; i < poss.Count; i++)
                 {
-                    if (poss[i].pos.x > maxX)
-                        maxX = poss[i].pos.x;
-                    else if (poss[i].pos.x < minX)
-                        minX = poss[i].pos.x;
+                    if (poss[i].pos.x + 1f > maxX)
+                        maxX = poss[i].pos.x + 1f;
+                    else if (poss[i].pos.x - 1f < minX)
+                        minX = poss[i].pos.x - 1f;
 
-                    if (poss[i].pos.y > maxY)
-                        maxY = poss[i].pos.y;
-                    else if (poss[i].pos.y < minY)
-                        minY = poss[i].pos.y;
+                    if (poss[i].pos.y + 1f > maxY)
+                        maxY = poss[i].pos.y + 1f;
+                    else if (poss[i].pos.y - 1f < minY)
+                        minY = poss[i].pos.y - 1f;
+                }
+
+                var hSpread = maxX - minX;
+                var hPosZ = Mathf.Clamp(-0.5f * hSpread / horiSin, -25f, -8f);
+                var vSpread = maxY - minY;
+                var vPosZ = Mathf.Clamp(-0.5f * vSpread / vertSin, -25f, -8f);
+                float posZ;
+
+                if (vPosZ > hPosZ)
+                {
+                    posZ = hPosZ;
+                    vSpread = posZ * vertSin * -2;
+                }
+                else
+                {
+                    posZ = vPosZ;
                 }
                 avg.x = (minX + maxX) * 0.5f;
-                minY = Mathf.Clamp(minY, -5f, 10f);
-                avg.y =  (minY+ maxY) * 0.5f;
-                avg.z = Mathf.Clamp(-0.5f * Vector2.Distance(new Vector2(minX, minY), new Vector2(maxX, maxY)) / lowerConst, -25f, -8f);
+
+                //minY = Mathf.Clamp(minY,vSpread * 0.5f + ViewportMinOffset, 10f);
+                avg.y = Mathf.Clamp((minY + maxY) * 0.5f, vSpread * 0.5f + ViewportMinOffset, 10f);
+                avg.z = posZ;
                 //CameraTrans.position = Vector3.MoveTowards(CameraTrans.position, avg, CamSpeed * Time.deltaTime);
                 TargetPos = avg;
             });
